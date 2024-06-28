036446f9de6c64cc0720ba2eff10054cc01d0b7a
Devaraj Sekar <officiallydevaraj@gmail.com>
2024-06-28 21:39:06 +0530

    Removed unsupported tic tac toe sample.

--- TicTacToe/TicTacToe/TicTacToe/TicTacToe/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
-
-[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
-
-[assembly: ExportFont("Poppins-ExtraBold.ttf", Alias = "poppinsextrabold")]
-[assembly: ExportFont("Poppins-Regular.ttf", Alias = "poppinsregular")]

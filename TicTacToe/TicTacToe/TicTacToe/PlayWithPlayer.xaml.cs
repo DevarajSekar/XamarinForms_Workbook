5f1c0369d3bf3786732ea4f8ae21bc52248b560c
Devaraj Sekar <officiallydevaraj@gmail.com>
2024-06-30 23:28:38 +0530

    TTT complete commit.

--- TicTacToe/TicTacToe/TicTacToe/PlayWithPlayer.xaml.cs
+++ TicTacToe/TicTacToe/TicTacToe/PlayWithPlayer.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.Forms;
@@ -14,11 +15,11 @@ namespace TicTacToe
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class PlayWithPlayer : ContentPage
     {
-        PlayWithFirentViewModel viewModel;
+        PlayWithFrientViewModel viewModel;
         public PlayWithPlayer()
         {
             InitializeComponent();
-            viewModel = new PlayWithFirentViewModel();
+            viewModel = new PlayWithFrientViewModel();
             this.BindingContext = viewModel;
             Application.Current.RequestedThemeChanged += (s, a) =>
             {
@@ -28,29 +29,158 @@ namespace TicTacToe
         }
     }
 
-    public class PlayWithFirentViewModel : INotifyPropertyChanged
+    public class PlayWithFrientViewModel : INotifyPropertyChanged
     {
         private Color individualBoxColor;
         private Color contentPageColor;
         private Color separatorLinesColor;
+        private Color textColor;
         private string[] arrays = new string[9];
         public ICommand TapCommand { get; set; }
+        public ICommand RestartCommand { get; set; }
+        string lasttext=string.Empty;
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public PlayWithFirentViewModel()
+        public PlayWithFrientViewModel()
         {
             TapCommand = new Command(AddValue);
+            RestartCommand = new Command(Restart);
             SetTheme();
+        }
+
+        private void Restart(object obj)
+        {
+            App.Current.MainPage.Navigation.PopAsync();
         }
 
         private void AddValue(object obj)
         {
             Button sender = (obj as Button);
-            arrays[Convert.ToInt32(sender.Text)] = "X";
-            sender.TextColor = Color.Black;
-            sender.Text = "X";
+            if (string.IsNullOrEmpty(lasttext))
+            {
+                lasttext = "X";
+            }
+            else if (lasttext == "X")
+                lasttext = "O";
+            else
+                lasttext = "X";
+
+            arrays[Convert.ToInt32(sender.Text)] = lasttext;
+            sender.TextColor = SeparatorLinesColor;
+            sender.Text = lasttext;
             sender.IsEnabled = false;
+
+            CheckResult();
+        }
+
+        private async void CheckResult()
+        {
+            string winner = "Player 1 Winner";
+            ////Row wise check
+            if (CheckRowsColsResult(0, 1, 2))
+            {
+                if (arrays[0] == "O")
+                    winner = "Player 2 Winner";
+                await App.Current.MainPage.DisplayAlert("Winner!!", winner, "OK");
+
+                await App.Current.MainPage.Navigation.PushAsync(new PlayWithPlayer());
+            }
+
+            ////Row wise check
+            else if (CheckRowsColsResult(3, 4, 5))
+            {
+                if (arrays[3] == "O")
+                    winner = "Player 2 Winner";
+                await App.Current.MainPage.DisplayAlert("Winner!!", winner, "OK");
+                await App.Current.MainPage.Navigation.PushAsync(new PlayWithPlayer());
+            }
+
+            ////Row wise check
+            else if (CheckRowsColsResult(6, 7, 8))
+            {
+                if (arrays[6] == "O")
+                    winner = "Player 2 Winner";
+                await App.Current.MainPage.DisplayAlert("Winner!!", winner, "OK");
+                await App.Current.MainPage.Navigation.PushAsync(new PlayWithPlayer());
+            }
+
+            ////Column wise check
+            else if (CheckRowsColsResult(0, 3, 6))
+            {
+                if (arrays[0] == "O")
+                    winner = "Player 2 Winner";
+                await App.Current.MainPage.DisplayAlert("Winner!!", winner, "OK");
+                await App.Current.MainPage.Navigation.PushAsync(new PlayWithPlayer());
+            }
+
+            ////Column wise check
+            else if (CheckRowsColsResult(1, 4, 7))
+            {
+                if (arrays[1] == "O")
+                    winner = "Player 2 Winner";
+                await App.Current.MainPage.DisplayAlert("Winner!!", winner, "OK");
+                await App.Current.MainPage.Navigation.PushAsync(new PlayWithPlayer());
+            }
+
+            ////Column wise check
+            else if (CheckRowsColsResult(2, 5, 8))
+            {
+                if (arrays[2] == "O")
+                    winner = "Player 2 Winner";
+                await App.Current.MainPage.DisplayAlert("Winner!!", winner, "OK");
+                await App.Current.MainPage.Navigation.PushAsync(new PlayWithPlayer());
+            }
+
+            ////Diagonal wise check
+            else if (CheckRowsColsResult(0, 4, 8))
+            {
+                if (arrays[0] == "O")
+                    winner = "Player 2 Winner";
+                await App.Current.MainPage.DisplayAlert("Winner!!", winner, "OK");
+                await App.Current.MainPage.Navigation.PushAsync(new PlayWithPlayer());
+            }
+
+            ////Diagonal wise check
+            else if (CheckRowsColsResult(2, 4, 6))
+            {
+                if (arrays[2] == "O")
+                    winner = "Player 2 Winner";
+                await App.Current.MainPage.DisplayAlert("Winner!!", winner, "OK");
+                await App.Current.MainPage.Navigation.PushAsync(new PlayWithPlayer());
+            }
+
+            ////Draw check
+            else if (CheckDrawresult())
+            {
+                await App.Current.MainPage.DisplayAlert("Draw", "Match Draw", "OK");
+                await App.Current.MainPage.Navigation.PushAsync(new PlayWithPlayer());
+            }
+        }
+
+        bool CheckRowsColsResult(int i, int j, int k)
+        {
+            if (!string.IsNullOrEmpty(arrays[i]) &&
+                !string.IsNullOrEmpty(arrays[j]) &&
+                !string.IsNullOrEmpty(arrays[k]) &&
+                arrays[i] == arrays[j] && arrays[j] == arrays[k])
+            {
+
+                return true;
+            }
+            return false;
+        }
+
+        bool CheckDrawresult()
+        {
+            foreach (string i in arrays)
+            {
+                if (string.IsNullOrEmpty(i))
+                {
+                    return false;
+                }
+            }
+            return true;
         }
 
         public void SetTheme()
@@ -80,6 +210,19 @@ namespace TicTacToe
             {
                 individualBoxColor = value;
                 OnPropertyChanged(nameof(IndividualBoxColor));
+            }
+        }
+
+        public Color TextColor
+        {
+            get
+            {
+                return textColor;
+            }
+            set
+            {
+                textColor = value;
+                OnPropertyChanged(nameof(TextColor));
             }
         }
 
